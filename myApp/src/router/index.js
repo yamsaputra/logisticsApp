702ef563eca0ecb5b4e46974deea7c365569e3df
unknown <yam_1304@ymail.com>
2025-01-13 20:30:52 +0100

    Deploying frontend cloud run. 3nd attempt.

--- myApp/src/router/index.js
+++ myApp/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
 import ionTabs from "../components/ion-tabs.vue";
-import homePage from "../views/homePage.vue";
-import trustPage from "../views/trustPage.vue";
-import accountPage from "../views/accountPage.vue";
-import loginPage from "../views/loginPage.vue";
-import signupPage from "../views/signupPage.vue";
+import homePage from "../views/HomePage.vue";
+import trustPage from "../views/TrustPage.vue";
+import accountPage from "../views/AccountPage.vue";
+import loginPage from "../views/LoginPage.vue";
+import signupPage from "../views/SignupPage.vue";
 import routePage from "../views/routePage.vue";
-import packagePage from "../views/packagePage.vue";
+import packagePage from "../views/PackagePage.vue";
 import listPage from "../views/listPage.vue";
 
 const routes = [
