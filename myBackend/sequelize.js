f2a262b41b7fe095ea2715bc72c0497d1314f67f
Yama Saputra <yam_1304@ymail.com>
2025-02-07 17:16:08 +0100

    Attempt 4.

--- myBackend/sequelize.js
+++ myBackend/sequelize.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 // Loads environment variables from the .env file.
 dotenv.config();
 
+//
+
 // Database connection/configuration.
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
